737cc07266534b05d7304028fc48e5ee88936d2d
Gmrp2210 <gmrp22@hotmail.com>
2021-03-28 22:36:52 -0600

    Changes in modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //Components
-import { ModuleComponent } from './Components/module/module.component';
 import { UsersComponent } from './Components/users/users.component';
 import { ProductsComponent } from './Components/products/products.component';
 import { SaleComponent } from './Components/sale/sale.component';
 import { PurchaseComponent } from './Components/purchase/purchase.component';
 
 const routes: Routes = [
-  { path: 'Home', component: ModuleComponent },
   { path: 'Users', component: UsersComponent },
   { path: 'Products', component: ProductsComponent },
   { path: 'Sales', component: SaleComponent },
