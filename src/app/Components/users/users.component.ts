ce10d4ce136cd7e5860c94f8b5c5086e4c7089f3
Gmrp2210 <gmrp22@hotmail.com>
2021-04-28 22:33:35 -0600

    uSERS POST

--- src/app/Components/users/users.component.ts
+++ src/app/Components/users/users.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/users/user.service';
 import { User } from '../../interfaces/user';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 let products: User[] = [];
 @Component({
-    selector: 'app-users',
-    styleUrls: ['./users.component.css'],
-    templateUrl: './users.component.html',
-  })
+  selector: 'app-users',
+  styleUrls: ['./users.component.css'],
+  templateUrl: './users.component.html',
+})
 export class UsersComponent implements OnInit {
   p: User[] = [];
-  constructor(private UserService: UserService) {
+  UserForm: FormGroup;
+  constructor(
+    private UserService: UserService,
+    private formBuilder: FormBuilder
+  ) {
     this.getAllUsers();
+    this.UserForm = this.formBuilder.group({
+      nombre: '',
+      usuario: '',
+      password: '',
+    });
   }
 
   delete(id: number) {
-    this.UserService.deleteUser(id).subscribe(data=>{
-      this.getAllUsers()
+    this.UserService.deleteUser(id).subscribe((data) => {
+      this.getAllUsers();
     });
   }
 
@@ -25,12 +34,24 @@ export class UsersComponent implements OnInit {
       this.data = response;
     });
   }
+
+  createUser() {
+    const user = {
+      id: 0,
+      nombre: this.UserForm.get('nombre')?.value,
+      usuario: this.UserForm.get('usuario')?.value,
+      password: this.UserForm.get('password')?.value,
+    };
+    console.log(user);
+    this.UserService.createUser(user).subscribe((newuser) => {});
+    this.UserForm.reset();
+    this.ngOnInit();
+  }
+  resetTable() {
+    this.getAllUsers();
+  }
   ngOnInit(): void {}
-  displayedColumns: string[] = [
-    'nombre',
-    'usuario',
-    'Acciones',
-  ];
+  displayedColumns: string[] = ['nombre', 'usuario', 'Acciones'];
   columnsToDisplayP: string[] = this.displayedColumns.slice();
   data: User[] = products;
 }
