b63b1533c5c402869f0fb9f743fb0346ef81f36a
Gmrp2210 <gmrp22@hotmail.com>
2021-04-28 22:23:48 -0600

    Sale POST

--- src/app/Components/sale/sale.component.ts
+++ src/app/Components/sale/sale.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { VentaService } from 'src/app/services/ventas/venta.service';
 import { Venta } from '../../interfaces/venta';
+import { FormBuilder, FormGroup } from '@angular/forms';
 let venta: Venta[] = [];
 @Component({
-    selector: 'app-sale',
-    templateUrl: './sale.component.html',
-    styleUrls: ['./sale.component.css']
-  })
+  selector: 'app-sale',
+  templateUrl: './sale.component.html',
+  styleUrls: ['./sale.component.css'],
+})
 export class VentasComponent implements OnInit {
-  constructor(private VentasServices: VentaService) {
+  p: Venta[] = [];
+  SaleForm: FormGroup;
+  constructor(
+    private VentasServices: VentaService,
+    private formBuilder: FormBuilder
+  ) {
     this.getAllVentas();
+    this.SaleForm = this.formBuilder.group({
+      fecha: '',
+      usuario: '',
+      total: '',
+      estado: '',
+    });
   }
   getAllVentas() {
     this.VentasServices.getAllVentas().subscribe((response: any) => {
@@ -19,11 +31,26 @@ export class VentasComponent implements OnInit {
   }
 
   delete(id: number) {
-    this.VentasServices.deleteVenta(id).subscribe(data=>{
-      this.getAllVentas()
+    this.VentasServices.deleteVenta(id).subscribe((data) => {
+      this.getAllVentas();
     });
   }
 
+  createSale() {
+    const sale = {
+      id: 0,
+      fecha: this.SaleForm.get('fecha')?.value,
+      usuario: this.SaleForm.get('usuario')?.value,
+      total: this.SaleForm.get('total')?.value,
+      estado: this.SaleForm.get('estado')?.value,
+    };
+    this.VentasServices.createVenta(sale).subscribe((newsale) => {});
+    this.SaleForm.reset();
+    this.ngOnInit();
+  }
+  resetTable() {
+    this.getAllVentas();
+  }
   ngOnInit(): void {}
   displayedColumns: string[] = [
     'fecha',
